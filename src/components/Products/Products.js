361d2ccbfd8758f07241b8d7e0cf268437a93b2e
mk-hasan <mkhasan6891@gmail.com>
2022-04-10 03:53:45 +0600

    useFirebase hooks used in home,product components

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Products = () => {
+    const { user } = useFirebase();
     return (
         <div>
             <h4>Products</h4>
+            <h5>{user ? user.displayName : 'user not using'}</h5>
         </div>
     );
 };
