9c734588fe0af2fed5aca7d41f28d34d53dfda11
mk-hasan <mkhasan6891@gmail.com>
2022-04-10 02:20:02 +0600

    useFirbase hook used and onAuthStateChanged used in that

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
+import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
-
+            <h2>please login</h2>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <input type="email" name="" id="" placeholder='Your Email' />
+            <br />
+            <input type="password" name="" id="" placeholder='Password' />
+            <br />
+            <input type="submit" value="Login" />
         </div>
     );
 };
